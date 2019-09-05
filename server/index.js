1bc98d59f6ad8534340e4f6ceb7303ee6d0612dd
Michael Kim <miketkim@umich.edu>
2019-09-04 17:18:22 -0700

    code cleanup

--- server/index.js
+++ server/index.js
@@ -8,9 +8,4 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-// app.get('/api/listing/:id', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public/index.html'));
-//   })
-
-
 app.listen(port, () => console.log(`App listening on port ${port}`));
