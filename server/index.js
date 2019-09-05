63a09f3b206bf3014f289126cd4d6d4234603482
Michael Kim <miketkim@umich.edu>
2019-09-04 17:16:13 -0700

    Rendering 3 components

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 const express = require('express'); 
-const axios = require('axios')
 
 const app = express();
-const port = 4000; 
+const port = 3005; 
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
+
+// app.get('/api/listing/:id', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public/index.html'));
+//   })
 
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
